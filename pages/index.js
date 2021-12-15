07ca1080ac6035d21dedea7654e78f6eceffae60
Edson De Carvalho <76922937+edsonDeCavalho@users.noreply.github.com>
2021-12-15 10:50:06 +0100

    Exercice 1 ajouter taches a la list

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import React,{useState} from "react";
 
 export default function Home() {
+    /**
+     * declaration de variable
+     */
+    const [count,setCount]=useState(0)
+    const [todoList,setTodoList] = useState(["task1","task2","task3"])
+    const [inter,setInter] = useState(" ")
+    console.log(count);
   return (
     <div className={styles.container}>
       <Head>
@@ -12,46 +20,29 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          <h1 className={styles.title}>
+             Hello Edson
+          </h1>
+          <h3>count : {count}</h3>
+          <button  onClick={ () => setCount(count+1)}>Button</button>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+          <input type="text" onChange={event => setInter(event.target.value)}></input>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <button  onClick={ () => setTodoList([...todoList, inter])}>Button</button>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          { todoList.length > 1 &&(
+              todoList.map((todo,i) =>{
+                  return (
+                      <div>
+                          <p key={i}>{todo}</p>
+                          <button  key={i} type="button" onClick={() =>setTodoList()}>delate task {i}</button>
+                      </div>)
+              })
+          )}
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+
       </main>
-
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
